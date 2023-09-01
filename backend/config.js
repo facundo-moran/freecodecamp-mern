f6d1941df168223730166fc10d9d6371c344f44b
Morandev <moranfcnddev@gmail.com>
2023-08-31 22:21:18 -0300

    add: mongoose config

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,7 @@
-export const PORT = 3000;
+import { config } from 'dotenv'
+
+//DOTENV
+config();
+
+export const PORT = parseInt(process.env.PORT) || 3000;
+export const MONGO_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_COLLECTION}?retryWrites=true&w=majority`;
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { MONGO_URL, PORT } from './config.js';
+import mongoose from 'mongoose'
 
+//DB
+mongoose.connect(MONGO_URL).then(() => {
+    console.log("mongoose successful connection");
+    //SERVER
+    app.listen(PORT, () => { console.log(`API Listening at localhost:${PORT}`) })
+}).catch(console.error)
+
+//EXPRESS
 const app = express();
 
 //ROUTES
 app.get('/', (req, res) => {
-    res.send("Hello world from NodeJS 18"); 
+    res.send("Hello world from NodeJS 18");
 })
 
-//SERVER
-app.listen(PORT, () => {console.log(`API Listening at localhost:${PORT}`)})
+
